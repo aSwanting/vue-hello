c98861c190e9e945d35592c2a939ae8f63192140
aSwanting <gab.damico@gmail.com>
2023-10-23 19:24:19 +0200

    add: further testing, classes, inline styles

--- js/script.js
+++ js/script.js
@@ -7,8 +7,32 @@ createApp({
     return {
       pageHeader: 'Vue Test Page',
       imageSRC: "https://picsum.photos/200",
-      classes: ["header-text", "rounded-img", "body-text"],
-      num: 0
+
+      classes: [
+        "header-text",
+        "rounded-img",
+        "body-text",
+        "test-box"
+      ],
+
+      flexWrapper: {
+        display: "flex",
+        gap: "40px",
+        padding: "0px 30px"
+      },
+
+      flexButton: {
+        flexGrow: 1,
+        cursor: "pointer",
+      },
+
+      num: 0,
+
+
+
+
+
+      
     }
   },
   methods: {
