a0dcf36bf77e1de2842b8a4cd4011ee633ac5c0a
aSwanting <gab.damico@gmail.com>
2023-10-24 01:43:35 +0200

    add: bouncing circles in test box

---
+++ js/circle.js
@@ -0,0 +1,90 @@
+class Circle {
+
+    constructor(d, x, y, location, boundary) {
+
+        // Random values
+        this.d = d
+        this.r = this.d / 2
+        this.x = x
+        this.y = y
+        this.dx = this.getDirection(-5, 5)
+        this.dy = this.getDirection(-5, 5)
+
+        // Append location
+        this.location = location
+
+        // DOM Element
+        this.element = this.buildCircle()
+
+        // Boundaries
+        this.boundary = boundary
+    }
+
+    buildCircle() {
+
+        // Create Circle
+        const circle = document.createElement("div")
+
+        // Circle Coordinates
+        circle.style.width = this.d + "px"
+        circle.style.translate = "-50% -50%"
+        circle.style.left = this.x + "px"
+        circle.style.top = this.y + "px"
+
+        // Circle Style
+        circle.style.position = "absolute"
+        circle.style.aspectRatio = 1
+        circle.style.borderRadius = "50%"
+        circle.style.border = this.d / 6 + "px solid"
+
+        // Append Circle
+        this.location.append(circle)
+
+
+        return circle
+
+    }
+
+    // Get Random Direction, Exclude 0
+    getDirection(min, max) {
+
+        let n = 0
+        while (!n) { n = rand(min, max) }
+        return n
+    }
+
+    // Move Circle, Bounce against Window Boundaries
+    moveCircle() {
+
+        // Right Boundary
+        if (this.x + this.dx > this.boundary.clientWidth - this.r) {
+            this.x = this.boundary.clientWidth - this.r
+            this.dx = -this.dx
+        }
+
+        // Left Boundary
+        if (this.x + this.dx < this.r) {
+            this.x = this.r
+            this.dx = -this.dx
+        }
+
+        // Bottom Boundary
+        if (this.y + this.dy > this.boundary.clientHeight - this.r) {
+            this.y = this.boundary.clientHeight - this.r
+            this.dy = -this.dy
+        }
+
+        // Top Boundary
+        if (this.y + this.dy < this.r) {
+            this.y = this.r
+            this.dy = -this.dy
+        }
+
+        // Increment Position
+        this.x += this.dx
+        this.y += this.dy
+        this.element.style.left = this.x + "px"
+        this.element.style.top = this.y + "px"
+
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
+///////////////////////////////////////// FUNCTION /////////////////////////////////////////
+
 // Random Number Function
 function rand(min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
-const createApp = Vue.createApp
+
+///////////////////////////////////////// VUE /////////////////////////////////////////
+
+const { createApp } = Vue
 
 createApp({
+
   data() {
     return {
       pageHeader: 'Vue Test Page',
-      imageSRC: "https://picsum.photos/200",
+      imageSRC: "./img/northen-lights.jpg",
 
       classes: [
         "header-text",
@@ -21,31 +27,23 @@ createApp({
 
       flexWrapper: {
         display: "flex",
-        gap: "40px",
-        padding: "0px 30px"
+        justifyContent: "center",
+        gap: "15px",
       },
 
       flexButton: {
-        flexGrow: 1,
+        flexBasis: "20%",
+        padding: "5px",
         cursor: "pointer",
+        fontSize: "14px",
+        fontWeight: "bold",
+        borderRadius: "50px",
       },
 
       num: 0,
 
+      circles: []
 
-      // circle: {
-      //   width: "30px",
-      //   // translate: "-50% -50%",
-      //   left: 0,
-      //   top: 0,
-      //   position: "absolute",
-      //   aspectRatio: 1,
-      //   borderRadius: "50%",
-      //   backgroundColor: "black",
-      // }
-
-      dx: 1,
-      dy: 1,
     }
   },
 
@@ -55,70 +53,42 @@ createApp({
     },
 
     addCircle() {
-
       const testBox = document.getElementById("test-box")
-
       const d = rand(20, 40)
-      const r = d / 2
-      let x = rand(r, testBox.clientWidth - r)
-      let y = rand(r, testBox.clientHeight - r)
-
-      console.log(d, r)
-
-      // Create Circle
-      const circle = document.createElement("div")
-      circle.id = "circle"
-
-      // Circle Coordinates
-      circle.style.width = d + "px"
-      circle.style.translate = "-50% -50%"
-      circle.style.left = x + "px"
-      circle.style.top = y + "px"
-
-      // Circle Style
-      circle.style.position = "absolute"
-      circle.style.aspectRatio = 1
-      circle.style.borderRadius = "50%"
-      circle.style.border = d / 10 + "px solid black"
-
-      // Draw Circle
-      testBox.append(circle)
-
-      // // Move Circle
-      // if (x > testBox.clientWidth - r) {
-      //   this.dx = -this.dx
-      // }
-
-      // if (x < r) {
-      //   this.dx = -this.dx
-      // }
-
-      // if (y > testBox.clientHeight - r) {
-      //   this.dy = -this.dy
-      // }
-      // if (y < r) {
-      //   this.dy = -this.dy
-      // }
-
-      // x += this.dx
-      // y += this.dy
-      // circle.style.left = x + "px"
-      // circle.style.top = y + "px"
-
-      return circle
+      let x = rand(d / 2, testBox.clientWidth - d / 2)
+      let y = rand(d / 2, testBox.clientHeight - d / 2)
+      this.circles.push(new Circle(d, x, y, testBox, testBox))
 
     },
 
     removeCircle() {
-
-      const circle = document.getElementById("circle")
-      if (circle) {
-        circle.remove()
+      let lastCircle
+      if (this.circles.length > 0) {
+        lastCircle = this.circles[this.circles.length - 1].element
+        lastCircle.remove()
+        this.circles.pop()
       }
+    },
 
+    clearCircles() {
+      const testBox = document.getElementById("test-box")
+      testBox.innerHTML = ""
+      this.circles = []
+    },
+
+    animate() {
+      this.circles.forEach(circle => {
+        circle.moveCircle()
+      });
+      requestAnimationFrame(this.animate)
     }
 
+  },
 
-  }
+  mounted() {
+    this.addCircle()
+    requestAnimationFrame(this.animate)
+  },
+
 }).mount('#app')
 
